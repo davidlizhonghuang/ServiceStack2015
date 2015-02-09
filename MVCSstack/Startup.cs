1c275e970965c8fc46969635a944503a35a0ae2e
lizhonghuangGit <lihonghuang@yahoo.com.au>
2015-02-10 08:53:26 +1100

    default

---
+++ MVCSstack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCSstack.Startup))]
+namespace MVCSstack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
